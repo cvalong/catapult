8fbb013b9d6d2d598988681c21705ecab972938e
Christopher Long <93620761+cvalong@users.noreply.github.com>
2026-03-13 10:53:13 -0700

    chore: remove old root-level backend (moved into template/)

--- src/lib/license.ts
+++
@@ -1,6 +0,0 @@
-import { randomBytes } from 'crypto'
-
-export function generateKey(): string {
-  const segment = () => randomBytes(2).toString('hex').toUpperCase()
-  return `CTPLT-${segment()}-${segment()}-${segment()}-${segment()}`
-}
